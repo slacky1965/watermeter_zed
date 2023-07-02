245cf7b1d75cf2eaa57c13dc05471275663d86d0
Fedor <lfa1@yandex.ru>
2023-07-02 18:41:40 +0300

    Update project.mk

--- project.mk
+++ project.mk
@@ -9,8 +9,7 @@ $(OUT_PATH)/$(SRC_PATH)/app_ui.o \
 $(OUT_PATH)/$(SRC_PATH)/watermeter.o \
 $(OUT_PATH)/$(SRC_PATH)/watermeterEpCfg.o \
 $(OUT_PATH)/$(SRC_PATH)/zb_appCb.o \
-$(OUT_PATH)/$(SRC_PATH)/zcl_watermeterCb.o \
-$(OUT_PATH)/$(SRC_PATH)/zcl_watermeterCfg.o
+$(OUT_PATH)/$(SRC_PATH)/zcl_watermeterCb.o
 
 # Each subdirectory must supply rules for building sources it contributes
 $(OUT_PATH)/$(SRC_PATH)/%.o: $(SRC_PATH)/%.c 
