a2fe6efa98847f3d4ec0cd7f4eeb7ab5dcd84a77
Fedor <lfa1@yandex.ru>
2023-07-09 01:17:01 +0300

    Update board_8258_diy.h

--- src/include/board_8258_diy.h
+++ src/include/board_8258_diy.h
@@ -2,12 +2,12 @@
 #define SRC_INCLUDE_BOARD_8258_DIY_H_
 
 /************************* Configure KEY GPIO ***************************************/
-#define BUTTON                  GPIO_PA7
-#define PA7_INPUT_ENABLE        ON
-#define PA7_DATA_OUT            OFF
-#define PA7_OUTPUT_ENABLE       OFF
-#define PA7_FUNC                AS_GPIO
-#define PULL_WAKEUP_SRC_PA7     PM_PIN_PULLUP_1M
+#define BUTTON                  GPIO_PB6
+#define PB6_INPUT_ENABLE        ON
+#define PB6_DATA_OUT            OFF
+#define PB6_OUTPUT_ENABLE       OFF
+#define PB6_FUNC                AS_GPIO
+#define PULL_WAKEUP_SRC_PB6     PM_PIN_PULLUP_1M
 
 #define PM_WAKEUP_LEVEL         PM_WAKEUP_LEVEL_LOW // only for KEY
 
@@ -31,11 +31,33 @@
 #define PB4_FUNC                AS_GPIO
 #define PULL_WAKEUP_SRC_PB4     PM_PIN_PULLUP_1M
 
-/**************************** Configure UART ***************************************/
+/**************************** Configure UART ***************************************
+*    UART_TX_PA2 = GPIO_PA2,
+*    UART_TX_PB1 = GPIO_PB1,
+*    UART_TX_PC2 = GPIO_PC2,
+*    UART_TX_PD0 = GPIO_PD0,
+*    UART_TX_PD3 = GPIO_PD3,
+*    UART_TX_PD7 = GPIO_PD7,
+*    UART_RX_PA0 = GPIO_PA0,
+*    UART_RX_PB0 = GPIO_PB0,
+*    UART_RX_PB7 = GPIO_PB7,
+*    UART_RX_PC3 = GPIO_PC3,
+*    UART_RX_PC5 = GPIO_PC5,
+*    UART_RX_PD6 = GPIO_PD6,
+*/
+
 #if UART_PRINTF_MODE
-#define DEBUG_INFO_TX_PIN       UART_TX_PB1//print
+#define DEBUG_INFO_TX_PIN       UART_TX_PD7//print
 #define BAUDRATE                115200
 #endif /* UART_PRINTF_MODE */
+
+/**************************** Configure LED ******************************************/
+
+#define LED1                        GPIO_PB1
+#define PB1_FUNC                    AS_GPIO
+#define PB1_OUTPUT_ENABLE           ON
+#define PB1_INPUT_ENABLE            OFF
+
 
 /************************* For 512K Flash only ***************************************/
 /* Flash map:
