4f8d38fb2620ddafbc4b0ecb131610d2d62fa71e
Fedor <lfa1@yandex.ru>
2024-10-18 18:17:00 +0300

    Update app_cfg.h

--- src/include/app_cfg.h
+++ src/include/app_cfg.h
@@ -62,14 +62,14 @@ extern "C" {
 #include "version_cfg.h"
 
 /* Debug mode config */
-#define	UART_PRINTF_MODE                OFF
+#define	UART_PRINTF_MODE                ON
 #define USB_PRINTF_MODE         		OFF
 
 #define DEBUG_CONFIG                    OFF
 #define DEBUG_WATERLEAK                 OFF
 #define DEBUG_REPORTING                 OFF
-#define DEBUG_BUTTON                    OFF
-#define DEBUG_COUNTER                   OFF
+#define DEBUG_BUTTON                    ON
+#define DEBUG_COUNTER                   ON
 #define DEBUG_BATTERY                   OFF
 #define DEBUG_PM                        OFF
 #define DEBUG_OTA                       OFF
